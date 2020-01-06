818dd3af48ca1ccdbd7cff6829d0f32e74440952
Dario Breitenstein <online@imakethings.ch>
2020-01-06 23:28:40 +0100

    verify

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ let currentPlayer = null
 makeTTSRequest('Ende der Nachricht. Wählen Sie ihre gewünschte Reaktion oder wählen sie Raute, um die Nachricht erneut abzuspielen.')
   .then(filename => {
     playSound(filename)
-    setTimeout(() => { playSound(filename) }, 2000)
+    setTimeout(() => { playSound(filename) }, 1000)
   })
 
 function playSound (filename) {
@@ -14,12 +14,13 @@ function playSound (filename) {
     if (currentPlayer) currentPlayer.kill('SIGINT')
     currentPlayer = spawn('/usr/bin/mplayer', ['-ao', 'alsa:device=plug=dmix', filename])
     currentPlayer.stderr.on('data', (data) => {
-      console.error(`[PLAYER] stderr:\n${data}`)
+      console.log(`[PLAYER] stderr:\n${data}`)
     })
     currentPlayer.on('close', function (code) {
       currentPlayer = null
-      if (code > 0) reject(new Error('Process failed with code '  + code))
-      else resolve()
+      //if (code > 0) reject(new Error('Process failed with code '  + code))
+      //else resolve()
+      resolve()
     })
   }))
 }
