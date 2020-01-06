a0092e523cbe6831e18f11d816297b32924e9e8e
Dario Breitenstein <online@imakethings.ch>
2020-01-06 23:24:23 +0100

    refactor currentsound

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process'
 import { makeTTSRequest } from './tts.js'
 
-let currentSound = null
+let currentPlayer = null
 
 makeTTSRequest('Ende der Nachricht. Wählen Sie ihre gewünschte Reaktion oder wählen sie Raute, um die Nachricht erneut abzuspielen.')
   .then(filename => {
@@ -11,13 +11,13 @@ makeTTSRequest('Ende der Nachricht. Wählen Sie ihre gewünschte Reaktion oder w
 
 function playSound (filename) {
   return new Promise(((resolve, reject) => {
-    if (currentSound) currentSound.kill('SIGINT')
-    const player = spawn('/usr/bin/mplayer', ['-ao', 'alsa:device=plug=dmix', filename])
-    player.stderr.on('data', (data) => {
+    if (currentPlayer) currentPlayer.kill('SIGINT')
+    currentPlayer = spawn('/usr/bin/mplayer', ['-ao', 'alsa:device=plug=dmix', filename])
+    currentPlayer.stderr.on('data', (data) => {
       console.error(`[PLAYER] stderr:\n${data}`)
     })
-    player.on('close', function (code) {
-      currentSound = null
+    currentPlayer.on('close', function (code) {
+      currentPlayer = null
       if (code > 0) reject(new Error('Process failed with code '  + code))
       else resolve()
     })
