84755b3ae48e6e949d00e33b97498637f41aa629
Dario Breitenstein <online@imakethings.ch>
2020-01-06 22:43:09 +0100

    add debug logs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import { makeTTSRequest } from './tts.js'
 makeTTSRequest('Ende der Nachricht. Ich liebe Sie.')
 
 const gstreamer = spawn('/usr/bin/gst-launch-1.0', ['filesrc location=54ea92a2754c510bb08ebf019a48ef4f.ogg ! oggdemux ! vorbisdec ! audioconvert ! audioresample ! alsasink'])
+gstreamer.stdout.on('data', (data) => {
+  console.log(`child stdout:\n${data}`)
+})
+
+gstreamer.stderr.on('data', (data) => {
+  console.error(`child stderr:\n${data}`)
+})
 gstreamer.on('close', function (code) {
   console.log('closed', code)
 });
