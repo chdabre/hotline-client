a86a4450c238d00a84992e9158341a9b704f3a38
Dario Breitenstein <online@imakethings.ch>
2020-01-06 22:46:48 +0100

    absolute path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { makeTTSRequest } from './tts.js'
 
 makeTTSRequest('Ende der Nachricht. Ich liebe Sie.')
 
-const gstreamer = spawn('/usr/bin/gst-launch-1.0', ['filesrc', 'location=54ea92a2754c510bb08ebf019a48ef4f.ogg', '!', 'oggdemux', '!', 'vorbisdec', '!', 'audioconvert', '!', 'audioresample', '!', 'alsasink'])
+const gstreamer = spawn('/usr/bin/gst-launch-1.0', ['filesrc', 'location=/root/fruendinne-hotline-client/54ea92a2754c510bb08ebf019a48ef4f.ogg', '!', 'oggdemux', '!', 'vorbisdec', '!', 'audioconvert', '!', 'audioresample', '!', 'alsasink'])
 gstreamer.stdout.on('data', (data) => {
   console.log(`child stdout:\n${data}`)
 })
