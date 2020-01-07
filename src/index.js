dcb362afe2519c4940bd5a6db712d9c2a852f176
Dario Breitenstein <online@imakethings.ch>
2020-01-07 17:27:22 +0100

    add wiring-op

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
-import { spawn } from 'child_process'
-import { makeTTSRequest } from './tts.js'
+// import { makeTTSRequest } from './tts.js'
+//
+// makeTTSRequest('Ende der Nachricht. Wählen Sie ihre gewünschte Reaktion oder wählen sie Raute, um die Nachricht erneut abzuspielen.')
+//   .then(filename => {
+//     playSound(filename)
+//     setTimeout(() => { playSound(filename) }, 2000)
+//   })
 
-let currentPlayer = null
+import wpi from 'wiring-op'
 
-makeTTSRequest('Ende der Nachricht. Wählen Sie ihre gewünschte Reaktion oder wählen sie Raute, um die Nachricht erneut abzuspielen.')
-  .then(filename => {
-    playSound(filename)
-    setTimeout(() => { playSound(filename) }, 2000)
-  })
-
-function playSound (filename) {
-  return new Promise(((resolve, reject) => {
-    if (currentPlayer) currentPlayer.kill('SIGINT')
-    currentPlayer = spawn('/usr/bin/mplayer', ['-ao', 'alsa:device=plug=dmix', filename])
-    currentPlayer.stderr.on('data', (data) => {
-      console.log(`[PLAYER] stderr:\n${data}`)
-    })
-    currentPlayer.on('close', function (code) {
-      currentPlayer = null
-      //if (code > 0) reject(new Error('Process failed with code '  + code))
-      //else resolve()
-      resolve()
-    })
-  }))
-}
+wpi.setup('wpo')
---
+++ src/sound.js
@@ -0,0 +1,24 @@
+import { spawn } from "child_process"
+
+class SoundManager {
+  constructor (props) {
+    this._currentPlayer = null
+  }
+
+  playSound (filename) {
+    const self = this
+    return new Promise(((resolve, reject) => {
+      if (this._currentPlayer) this._currentPlayer.kill('SIGINT')
+      this._currentPlayer = spawn('/usr/bin/mplayer', ['-ao', 'alsa:device=plug=dmix', filename])
+      this._currentPlayer.stderr.on('data', (data) => {
+        console.log(`[PLAYER] stderr:\n${data}`)
+      })
+      this._currentPlayer.on('close', function (code) {
+        self._currentPlayer = null
+        //if (code > 0) reject(new Error('Process failed with code '  + code))
+        //else resolve()
+        resolve(code)
+      })
+    }))
+  }
+}
