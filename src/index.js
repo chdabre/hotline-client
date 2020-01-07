22c7011b4aea663a993287cabd36c49d86bcd06b
Dario Breitenstein <online@imakethings.ch>
2020-01-07 17:37:40 +0100

    gpio test

--- src/index.js
+++ src/index.js
@@ -9,3 +9,19 @@
 import wpi from 'wiring-op'
 
 wpi.setup('wpo')
+
+const DIAL_PIN = 8
+const PULSE_PIN = 9
+
+wpi.pinMode (DIAL_PIN, wpi.INPUT)
+wpi.pinMode (PULSE_PIN, wpi.INPUT)
+wpi.pullUpDnControl (DIAL_PIN, wpi.PUD_UP)
+wpi.pullUpDnControl (PULSE_PIN, wpi.PUD_UP)
+
+wpi.wiringPiISR(DIAL_PIN, wpi.INT_EDGE_FALLING, function(delta) {
+  console.log('DIAL pin changed to LOW (', delta, ')');
+})
+
+wpi.wiringPiISR(PULSE_PIN, wpi.INT_EDGE_FALLING, function(delta) {
+  console.log('PULSE pin changed to LOW (', delta, ')');
+})
