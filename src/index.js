72a2bd4360102e42a6949947302a8d19dbd9d946
Dario Breitenstein <online@imakethings.ch>
2020-01-07 18:40:51 +0100

    interrupts?

--- src/index.js
+++ src/index.js
@@ -13,16 +13,24 @@ const DIAL_PIN_BCM = 19
 const PULSE_PIN_BCM = 18
 
 const dialPin = new Gpio(DIAL_PIN_BCM, 'in')
-const pulsePin = new Gpio(PULSE_PIN_BCM, 'in')
+// const pulsePin = new Gpio(PULSE_PIN_BCM, 'in')
+const pulsePin = new Gpio(PULSE_PIN_BCM, 'in', 'both', { debounceTimeout: 50 })
 
-const watchInterval = setInterval(() => {
-  dialPin.read().then(value => {
-    console.log('Dial Pin Value: ', value)
-  })
-}, 50)
+pulsePin.watch((err, value) => {
+  if (err) {
+    throw err
+  }
+  console.log(`PULSE Value changed to ${value}`)
+})
+
+// const watchInterval = setInterval(() => {
+//   dialPin.read().then(value => {
+//     console.log('Dial Pin Value: ', value)
+//   })
+// }, 50)
 
 process.on('SIGINT', _ => {
   dialPin.unexport()
   pulsePin.unexport()
-  clearInterval(watchInterval)
+  // clearInterval(watchInterval)
 })
