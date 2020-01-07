70cbab415ab808fffee799f2494feb462d1c2518
Dario Breitenstein <online@imakethings.ch>
2020-01-07 18:30:04 +0100

    clear interval

--- src/index.js
+++ src/index.js
@@ -15,13 +15,14 @@ const PULSE_PIN_BCM = 71
 const dialPin = new Gpio(DIAL_PIN_BCM, 'in')
 const pulsePin = new Gpio(PULSE_PIN_BCM, 'in')
 
-setInterval(() => {
+const watchInterval = setInterval(() => {
   dialPin.read().then(value => {
     console.log('Dial Pin Value: ', value)
   })
-})
+}, 50)
 
 process.on('SIGINT', _ => {
   dialPin.unexport()
   pulsePin.unexport()
+  clearInterval(watchInterval)
 })
