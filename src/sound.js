b7a71078b5ab4e92ffde283d8722f0df22043a59
Dario Breitenstein <online@imakethings.ch>
2020-01-08 22:49:18 +0100

    maybe

--- src/sound.js
+++ src/sound.js
@@ -24,16 +24,16 @@ export default class SoundManager {
     return new Promise(((resolve, reject) => {
       if (this.isPlaying()) this.stopAll()
 
-      let player;
+      let player, decoder;
       if (!mplayer) {
-        const decoder = spawn('/usr/bin/opusdec', ['--force-wav', '--quiet', filename, '-'])
+        decoder = spawn('/usr/bin/opusdec', ['--force-wav', '--quiet', filename, '-'])
         player = spawn('/usr/bin/aplay', ['-Dplug:dmix'])
         decoder.stdout.pipe(player.stdin)
-        this._currentPlayers.push(decoder)
       } else {
         player = spawn('/usr/bin/mplayer', ['-ao', 'alsa:device=plug=dmix', filename])
       }
       this._currentPlayers.push(player)
+      if (decoder) this._currentPlayers.push(decoder)
 
       player.on('close', function (code) {
         //if (code > 0) reject(new Error('Process failed with code '  + code))
