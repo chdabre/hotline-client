f2624f1ef3fb6feea4531562f1484418aa40582f
Dario Breitenstein <online@imakethings.ch>
2020-01-15 18:05:04 +0100

    date format

--- src/index.js
+++ src/index.js
@@ -128,7 +128,7 @@ class StateReadMessage extends PhoneState {
     const message = this._context.newMessages.shift()
     if (typeof message !== 'undefined') {
       this._context.currentMessage = message
-      this._context.soundManager.playSoundTTS(`Nachricht vom ${ new Date(message.date).toLocaleString() }`)
+      this._context.soundManager.playSoundTTS(`Nachricht vom ${ new Date(message.date).x.toLocaleString('de', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric' }) }`)
         .then(() => this._context.soundManager.playSound(message.url, true))
         .then(() => this._context.setState(new StateExpectResponse(this._context)))
         .catch(() => {})
