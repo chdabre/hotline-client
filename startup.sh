58d57461608272584d4e3ca80b9f8525d592fdd8
Dario Breitenstein <online@imakethings.ch>
2020-02-12 07:57:22 +0100

    add setup files

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export PATH=/root/.nvm/versions/node/v13.5.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+export GOOGLE_APPLICATION_CREDENTIALS=/root/google-credentials.json
+
+cd /root/hotline-client || exit
+git pull
+yarn
+
+killall aplay
+killall mplayer
+killall node
+aplay -Dplug:dmix -r 8000 -f S16_LE /dev/zero &
+yarn start
