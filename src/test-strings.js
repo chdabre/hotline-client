4e983ab32cbb6ea54914247a8dd322e61cb55c19
Dario Breitenstein <online@imakethings.ch>
2020-03-02 14:14:43 +0100

    fix encoding

--- src/test-strings.js
+++ src/test-strings.js
@@ -1,17 +1,26 @@
 import i18n from 'i18n'
 
 i18n.configure({
+  default_locale: 'de_normal',
   directory: 'src/lang'
 })
 
-i18n.setLocale('de_uplifting')
+i18n.getLocales().forEach(loc => {
+  try {
+    i18n.setLocale(loc)
+    console.log(`[${loc}]`)
 
-console.log(i18n.__('voice'))
-console.log(i18n.__n('greeting', 'greeting', 0))
-console.log(i18n.__n('greeting', 'greeting', 1))
-console.log(i18n.__n('greeting', 'greeting', 15))
-console.log(i18n.__('messageHeader', new Date().toLocaleString('de')))
-console.log(i18n.__('noMoreMessages'))
-console.log(i18n.__('endOfMessage'))
-console.log(i18n.__('updateAvailable'))
+    console.log(i18n.__('voice'))
+    console.log(i18n.__n('greeting', 'greeting', 0))
+    console.log(i18n.__n('greeting', 'greeting', 1))
+    console.log(i18n.__n('greeting', 'greeting', 15))
+    console.log(i18n.__('messageHeader', new Date().toLocaleString('de')))
+    console.log(i18n.__('noMoreMessages'))
+    console.log(i18n.__('endOfMessage'))
+    console.log(i18n.__('updateAvailable'))
 
+    console.log('\n')
+  } catch (e) {
+    console.error('Error in locale ' + loc + ':' + e)
+  }
+})
