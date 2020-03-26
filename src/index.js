34b48ecd0a1c1fd1f4bcbc125625652d8fc381be
Dario Breitenstein <online@imakethings.ch>
2020-03-26 16:45:45 +0100

    make everything async

--- src/index.js
+++ src/index.js
@@ -117,8 +117,7 @@ class PhoneContext {
   }
 
   formatTimeago (date) {
-    console.log(i18n.getLocale().substr(0,2))
-    const timeAgo = new TimeAgo(i18n.getLocale().substr(0,2) === 'de' ? 'de' : 'en')
+    const timeAgo = new TimeAgo(i18n.getLocale().substr(0,2))
     return timeAgo.format(date)
   }
 
